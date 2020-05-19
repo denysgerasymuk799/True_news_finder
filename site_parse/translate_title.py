c946c32df910b1c0569a009c2f08fbb42c3f4a9c
Denys <herasymuk@ucu.edu.ua>
2020-05-19 13:38:31 +0300

    nltk4

---
+++ site_parse/translate_title.py
@@ -0,0 +1,25 @@
+import json
+import time
+
+from googletrans import Translator
+
+
+def translate_title(article_title):
+    translator = Translator()
+    try:
+        src_lang = translator.translate(article_title).src
+    except json.decoder.JSONDecodeError:
+        time.sleep(3)
+        translator = Translator()
+        src_lang = translator.translate(article_title).src
+
+    # REINITIALIZE THE API
+    translator = Translator()
+    try:
+        translated = translator.translate(article_title, src=src_lang, dest="en")
+        article_title_en = translated.text
+    except Exception as e:
+        print(str(e))
+        article_title_en = ""
+
+    return article_title_en
